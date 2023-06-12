b71062de347828440d66d575e2d5d25392084b30
wbhaao <a31620504@gmail.com>
2023-06-13 08:23:29 +0900

    최종본

--- package.cpp
+++ package.cpp
@@ -18,6 +18,35 @@ long active[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 long screen[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int tmpCol = 0;
 
+//Define The Snake as a Struct
+typedef struct Snake Snake;
+struct Snake{
+  int head[2];     // the (row, column) of the snake head
+  int body[40][2]; //An array that contains the (row, column) coordinates
+  int len;         //The length of the snake 
+  int dir[2];      //A direction to move the snake along
+};
+
+//Define The Apple as a Struct
+typedef struct Apple Apple;
+struct Apple{
+  int rPos; //The row index of the apple
+  int cPos; //The column index of the apple
+};
+
+
+byte pic[8] = {0,0,0,0,0,0,0,0};//The 8 rows of the LED Matrix
+
+Snake snake = {{1,5},{{0,5}, {1,5}}, 2, {1,0}};//Initialize a snake object
+Apple apple = {(int)random(0,8),(int)random(0,8)};//Initialize an apple object
+
+//Variables To Handle The Game Time
+float oldTime = 0;
+float timer = 0;
+float updateRate = 3;
+
+int i,j;//Counters
+
 int figura = 0;
 int figuraNext = 0;
 int fromLeft = 0;
@@ -496,7 +525,7 @@ void jumpman(int cnt);
 // ---------------- 기본 동작 -------------
 int cnt = 0;
 int upPin = 4;
-int downPin = 8;
+int downPin = 3;
 
 int leftPin = 2;
 int rightPin = 5;
@@ -518,7 +547,7 @@ void setup() {
   pinMode(leftPin, INPUT);
   pinMode(rightPin, INPUT);
   // 2번핀이 INPUT되었을 떄 함수 실행, FALLING HIGH->LOW일때 인터럽트 실행
-  attachInterrupt(1, buttonPush, FALLING);
+  attachInterrupt(0, buttonPush, FALLING);
   lc.shutdown(0, false);
   lc.shutdown(1, false);
   lc.shutdown(2, false);
@@ -539,7 +568,7 @@ void setup() {
 
   randomSeed(analogRead(0));
 }
-
+void startSnake(int rightState, int leftState, int downState, int upState);
 void buttonControl(int upState, int downState, int leftState, int rightState);
 void loop() {
   cnt += 1;
@@ -588,7 +617,7 @@ void loop() {
 
     // 점프맨
     if (nowState == 1) {
-      jumpman(cnt);
+      startSnake(rightState, leftState, downState, upState);
     }
     // 테트리스
     else if (nowState == 2) {
@@ -604,7 +633,7 @@ void loop() {
 
 void whatGame() { //현재 무슨게임인지 알려주는 함수
   if (nowState == 1) {
-    lcd.print("null");
+    lcd.print("snake");
   } else if (nowState == 2) {
     lcd.print("jumpman");
   } else if (nowState == 3) {
@@ -1806,4 +1835,115 @@ void memorize_gameover(){
   delay(5000);
   inGame = 0;
   exit(0);
+}
+
+float calculateDeltaTime(){
+  float currentTime = millis();
+  float dt = currentTime - oldTime;
+  oldTime = currentTime;
+  return dt;
+}
+
+void reset(){
+  for(int j=0;j<8;j++){
+    pic[j] = 0;
+  }
+}
+void Update(){
+  reset();//Reset (Clear) the 8x8 LED matrix
+  
+  int newHead[2] = {snake.head[0]+snake.dir[0], snake.head[1]+snake.dir[1]};
+
+  //Handle Borders
+  if(newHead[0]==8){
+    newHead[0]=0;
+  }else if(newHead[0]==-1){
+    newHead[0] = 7;
+  }else if(newHead[1]==8){
+    newHead[1]=0;
+  }else if(newHead[1]==-1){
+    newHead[1]=7;
+  }
+  
+  //Check If The Snake hits itself
+   for(j=0;j<snake.len;j++){
+    if(snake.body[j][0] == newHead[0] && snake.body[j][1] == newHead[1]){
+      //Pause the game for 1 sec then Reset it
+      delay(1000);
+      snake = {{1,5},{{0,5}, {1,5}}, 2, {1,0}};//Reinitialize the snake object
+      apple = {(int)random(0,8),(int)random(0,8)};//Reinitialize an apple object
+      return;
+    }
+  }
+
+  //Check if The snake ate the apple
+  if(newHead[0] == apple.rPos && newHead[1] ==apple.cPos){
+    snake.len = snake.len+1;
+    apple.rPos = (int)random(0,8);
+
+    // 이거바꿈
+    apple.cPos = (int)random(0,8);
+  }else{
+    removeFirst();//Shifting the array to the left
+  }
+  
+  snake.body[snake.len-1][0]= newHead[0];
+  snake.body[snake.len-1][1]= newHead[1];
+  
+  snake.head[0] = newHead[0];
+  snake.head[1] = newHead[1];
+  
+  //Update the pic Array to Display(snake and apple)
+  for(j=0;j<snake.len;j++){
+    pic[snake.body[j][0]] |= 128 >> snake.body[j][1];
+  }
+  pic[apple.rPos] |= 128 >> apple.cPos;
+  
+}
+
+void Render(){
+  
+   for(i=0;i<8;i++){
+    lc.setRow(0,i,pic[i]);
+   }
+}
+
+void removeFirst(){
+  for(j=1;j<snake.len;j++){
+    snake.body[j-1][0] = snake.body[j][0];
+    snake.body[j-1][1] = snake.body[j][1];
+  }
+}
+void startSnake(int rightState, int leftState, int downState, int upState){
+    // put your main code here, to run repeatedly:
+  float deltaTime = calculateDeltaTime();
+  timer += deltaTime;
+  Serial.println("11111111111111111111111111111111111");
+  Serial.println(upState);
+  Serial.println(downState);
+  Serial.println(rightState);
+  Serial.println("11111111111111111111111111111111111");
+  
+  //Check For Inputs
+  if(rightState && snake.dir[1]==0){
+    snake.dir[0] = 0;
+    snake.dir[1] = -1;
+  }else if(leftState && snake.dir[1]==0){
+    snake.dir[0] = 0;
+    snake.dir[1] = 1;
+  }else if(downState && snake.dir[0]==0){
+    snake.dir[0] = -1;
+    snake.dir[1] = 0;
+  }else if(upState && snake.dir[0]==0){
+    snake.dir[0] = 1;
+    snake.dir[1] = 0;
+  }
+  
+  //Update
+  if(timer > 1000/updateRate){
+    timer = 0;
+    Update();
+  }
+  //Render
+  Render();
 }
